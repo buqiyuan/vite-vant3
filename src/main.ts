435c678881962c845e7648ddcbaeb49a94775c46
bqy_fe <1743369777@qq.com>
2021-06-20 17:36:20 +0800

    chore: update dvps

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import {setupRouter} from './router'
 
 const app = createApp(App)
 
+// if (import.meta.env.PROD) {
+//   import('vconsole').then((res) => {
+//     new res.default()
+//   })
+// }
+
 // 安装插件（vant-ui等）,若使用了 vite-plugin-components 插件，则需要手动引入组件
 setupPlugins(app)
 // 安装vuex
